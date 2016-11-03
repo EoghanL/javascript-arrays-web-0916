67af30f76cac520e798e6cd9182f8d8aa274e1c6
github username <github email address>
2016-11-02 22:13:49 -0400

    Done.

---
+++ arrays.js
@@ -0,0 +1,36 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+
+}
+
+function addElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function removeElementFromBeginningOfArray(array, element){
+  array.shift()
+  return array
+}
+
+function removeElementFromEndOfArray(array, element){
+  array.pop()
+  return array
+}
